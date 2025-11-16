a92481767560746a8e5d1bdc27cf0a6c0d384ad1
Allan <allanjaybusante3@gmail.com>
2025-11-16 15:56:52 +0800

    Update: convert to a template based html and analytics page

--- core/main/views.py
+++ core/main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+def analytics(request):
+    return render(request, 'main/analytics.html')
